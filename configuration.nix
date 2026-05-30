cb8bc23b374404f9c301352fffb7a18bb0cd28e5
dayu <dayu@dayu.jp>
2026-05-31 01:25:07 +0900

    auto optimise

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@
   # services.xserver.libinput.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dayu = {
